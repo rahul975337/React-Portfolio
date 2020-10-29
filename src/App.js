66f64a8bb088722ba341c8fb37ba1409e6901e2d
rahul975337 <tilwani03@gmail.com>
2020-10-30 03:24:25 +0530

    working on home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import MyCarousel from "./components/myCarousel/myCarousel";
 // components
 import MyNavBar from "./components/myNavBar/MyNavBar";
+import TitleMessage from "./components/title-message/title-message";
 const App = () => {
   return (
     <div className="App">
       <MyNavBar />
+      <MyCarousel/>
+      <TitleMessage/>
     </div>
   );
 };
